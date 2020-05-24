2c3c6436159d018623a3edbfd6eaf9c25c42794d
Piotr Kowalski <piecioshka@gmail.com>
2020-05-24 13:45:33 +0200

    5th workshop revision

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,6 +1,7 @@
 
 import { VideoPlayableFactory } from './videos/video-playable-factory.js';
 import { VideoReadonlyFactory } from './videos/video-readonly-factory.js';
+import { VideoPaidFactory } from './videos/video-paid-factory.js';
 
 function main() {
     console.log('home page');
@@ -11,6 +12,7 @@ function main() {
         const imageId = index * 10;
         const film = {
             playable: Math.random() > 0.5,
+            isPaid: Math.random() > 0.5,
             title: 'Random Title',
             thumb: `https://picsum.photos/id/${imageId}/300/150/`
         };
@@ -18,6 +20,8 @@ function main() {
         let $v = null;
         if (film.playable) {
             $v = VideoPlayableFactory.create(film);
+        } else if (film.isPaid) {
+            $v = VideoPaidFactory.create(film);
         } else {
             $v = VideoReadonlyFactory.create(film);
         }
